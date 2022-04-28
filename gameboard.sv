796fda2f5e170b437999d9befafbed8ff5c38e9f
Quinn <qbdoud@gmail.com>
2022-04-27 23:33:13 -0500

    gameboard mostly finished; start of state logic.

--- gameboard.sv
+++ gameboard.sv
@@ -1,16 +1,16 @@
 module gameboard (
     input logic [3:0]piece_buffer[3:0], //may need more bits
     input logic clk,
-    input logic [31:0]board_data
+    input logic [31:0]board_data[30]
     input logic ROTATELEFT,  ROTATERIGHT, FALL, MOVELEFT, MOVERIGHT, PIECEPLACED, CLEARLINECHECK, CLEARLINEACT, CLEARLINE, KONAMI, NONEINPUT, HOLDPIECE, ENDGAME, CLEARALL,
     output logic [10:0] RAM_ROW_ADDR
     output logic RAM_WE, RAM_RE,
-    input logic [31:0]RAM_READDATA
     output logic RAM_WRITEDATA
-    output logic [3:0]collision
-    output logic [3:0]sandboxLR
-    output logic [4:0]sandboxUD
-    output logic [3:0]sandbox[3:0]
+    output logic [3:0]collision,
+    output logic [3:0]sandboxLR,
+    output logic [4:0]sandboxUD,
+    output logic [3:0]sandbox[3:0],
+    output logic [5:0]clearlineval,
  );
 /*
 This module controls the rotations, translations, and collisions between board pieces and the board itself.
@@ -18,20 +18,20 @@ This module controls the rotations, translations, and collisions between board p
 
 
 */
-logic [15:0]interpreted_readdata
-logic [3:0]sandboxLR = 4'd5
-logic [4:0]sandboxUD = 5'd0
-logic [3:0]sandbox[3:0]
-logic [3:0]preliminarysandbox[3:0]
-int bitpair
+logic [15:0]interpreted_boarddata;
+logic [3:0]preliminarysandbox[3:0];
+logic [3:0]holdbox[3:0];
+logic [3:0]board_data_slice[3:0]
+int bitpair;
 for(bitpair = 0; bitpair < 32; bitpair = bitpair + 2)begin
-    interpreted_readdata[bitpair/2] = RAM_READDATA[bitpair] || RAM_READDATA[bitpair+1]
+    interpreted_boarddata[bitpair/2] = board_data[bitpair] || board_data[bitpair+1]
 end
-if(  PIECEPLACED ) begin
+if( PIECEPLACED ) begin
  sandbox = piece_buffer
 end
 
-if(  ROTATELEFT ) begin
+if( ROTATELEFT ) begin
+    //unfortunate but cheap rotation alg
 /*
 1 2 3 4            4 8 . .
 5 6 7 8            3 7 . .
@@ -42,12 +42,31 @@ if(  ROTATELEFT ) begin
     int j;
     for(i = 0; i < 4 ; i = i + 1)begin
         for(j = 0; j < 4; j = j + 1)begin
-        preliminarysandbox[i][j] = sandbox[i][3-j]
+            preliminarysandbox[i][j] = sandbox[i][3-j] 
+            if((((sandboxLR + j) < 4) || ((sandbox+j) > 34) || ((sandboxLR + i) < 4) || ((sandboxLR + i) > 14))) begin
+                board_data_slice[i][j] = 0
+            end
+            else begin
+                board_data_slice = board_data[i+sandboxUD][j+sandboxLR]
+            end
         end
     end
 end
-
-if(  ROTATERIGHT ) begin
+if(ROTATELEFT_2) begin
+    int i;
+    int j;
+    for (i = 0; i < 4; i = i+1)begin
+        for(j = 0; j < 4; j = j + 1)begin
+            if(preliminarysandbox[i][j] &&( board_data[i+sandboxUD][j+sandboxLR] || board_data[i+sandboxUD][j+sandboxLR + 1]))begin
+                collision[2] = 1'b1
+            end
+            else begin
+                sandbox[i][j] = preliminarysandbox[i][j]
+            end
+        end
+    end
+end
+if( ROTATERIGHT ) begin
 /*
 1 2 3 4            13 9 5 1
 5 6 7 8             . . 6 2
@@ -61,53 +80,92 @@ if(  ROTATERIGHT ) begin
         preliminarysandbox[i][j] = sandbox[3-i][j]
         end
     end
-
 end
-//If collision[x] == 1, then we won't collide when we try to move.
-if(  FALL ) begin
-    if(collision[0] == 1'b1)begin
+if(ROTATERIGHT_2) begin
+    int i;
+    int j;
+    for (i = 0; i < 4; i = i+1)begin
+        for(j = 0; j < 4; j = j + 1)begin
+            if(preliminarysandbox[i][j] &&( board_data[i+sandboxUD][j+sandboxLR] || board_data[i+sandboxUD][j+sandboxLR + 1]))begin
+                collision[2] = 1'b1
+            end
+            else begin
+                sandbox[i][j] = preliminarysandbox[i][j]
+            end
+        end
+    end
+end
+//If collision[x] == 0, then we won't collide when we try to move. Up/Down/Left/Right = 0/1/2/3
+if( FALL ) begin
+    if(collision[1] == 1'b0)begin
         sandboxUD = sandboxUD + 1
     end 
 end
 
-if(  MOVELEFT ) begin
-    if(collision[1] == 1'b1)begin
+if( MOVELEFT ) begin
+    if(collision[2] == 1'b0)begin
         sandboxLR = sandboxLR - 1
     end
 end
 
-if(  MOVERIGHT ) begin
-    if(collision[2] == 1'b1)begin
+if( MOVERIGHT ) begin
+    if(collision[3] == 1'b0)begin
         sandboxLR = sandboxLR + 1
     end
 end
 
-if(  KONAMI ) begin
-    if(collision[3] == 1'b1)begin
-        if(sandboxUD == 5'b00000) sandbox = 0
+if( KONAMI ) begin
+    if(collision[0] == 1'b0)begin
         else sandboxUD = sandboxUD - 1
     end
-end
-if(  NONEINPUT ) begin
+end 
+if( NONEINPUT ) begin
 //not really handled here.
 //basically, if there's no input then the block should lock in faster.
 end
 
-if(  CLEARLINECHECK )begin
+if( CLEARLINECHECK )begin
+  //clock fuckery
+    int i;
+    for(i = 0; i < 30; i = i+1)begin
+        if(interpretedboard_data[i] == 16'b1111111111111111)begin
+            clearlineval = i
+        end
+    end
+end
+
+if( CLEARLINEACT )begin
+    board_data[clearlineval:0] = board_data[clearlineval-1:0] //pulls values down
+    clearlineval = 6'b11111
+end
+
+if( CLEARLINE )begin
+    //doesn't happen here -- if clearlineval = 31 (outside range) then we loop back to check
+end
+
+if( HOLDPIECE )begin
+    holdbox = sandbox;
+    sandboxLR = DEFAULTPOSLR;
+    sandboxUD = DEFAULTPOSUD; //currently just moves ur piece back to top
+end
+
+if( HOLDPIECE_2)begin
+end
+ 
+if( ENDGAME )begin
 
 end
 
-if(  CLEARLINEACT )begin
-end
-
-if(  CLEARLINE )begin
-end
-
-if(  HOLDPIECE )begin
-end
-
-if(  ENDGAME )begin
-end
-
-if(  CLEARALL )begin
+if( CLEARALL )begin
+    int i;
+    for(i = 0; i<30; i = i+1)begin
+        board_data[i] = 0
+    end
+    int j
+    for(i = 0; i<4; i = i + 1)begin
+        for(j = 0; j < 4; j = j + 1)begin
+        holdbox[i][j] = 0
+        sandbox[i][j] = 0
+        end 
+    end
 end
--- statemachine.sv
+++ statemachine.sv
@@ -1,33 +1,116 @@
 module statemachine (
-    input [1:0] rotate,
-    input       clearline,
-    input [8:0] keyboardinput,
-    input       pieceplaced,
-    input       placepiece,
-    input       pogchamp
-    output [5:0] state
+    input  logic [1:0] rotate,
+    input  logic       clearline,
+    input  logic [8:0] keyboardinput,
+    input  logic       pieceplaced,
+    input  logic       placepiece,
+    input  logic       pogchamp,
+    input  logic       ground_notif,
+    input  logic [5:0] clearlineval,
+
+    output logic       FALL,
+                       MOVELEFT,
+                       MOVERIGHT,
+                       KONAMI,
+                       NONEINPUT
+                       CLEARLINECHECK,
+                       CLEARLINEACT,
+                       CLEARLINE,
+                       PIECEPLACED,
+                       ROTATELEFT,
+                       ROTATELEFT_2,
+                       ROTATERIGHT,
+                       ROTATERIGHT_2,
+                       HOLDPIECE,
+                       HOLDPIECE_2,
+                       HOLDPIECE_3,
+                       
+                       
 );
-    enum logic [5:0]{
-        logo,
-        insertpiece,
-        //FALL
-            rotatel
-            rotater
-            movel,
-            mover,
-            fall,
-            none,
-        pieceplaced,
-        clearlinecheck,
-        clearlineact,
-        clearline,
-        holdpiece,
-        endscreen,
-        clearall,
-
-    }
-
-
+    enum logic [4:0]{
+        //GAMEBOARD RELEVANT STATES
+        STATE_PIECEPLACED,
+        STATE_ROTATELEFT_1,
+        STATE_ROTATELEFT_2,
+        STATE_ROTATERIGHT,
+        STATE_ROTATERIGHT_2,
+        STATE_FALL,
+        STATE_FALL_2,
+        STATE_MOVELEFT,
+        STATE_MOVERIGHT,
+        STATE_KONAMI,
+        STATE_NONEINPUT,
+        STATE_CLEARLINECHECK,
+        STATE_CLEARLINEACT,
+        STATE_CLEARLINE,
+        STATE_HOLDPIECE,
+        STATE_HOLDPIECE_2,
+        STATE_HOLDPIECE_3,
+        STATE_ENDGAME,
+        STATE_CLEARALL,
+        //
+        //OTHER STATES:
+        //
+        STATE_PIECE_LOAD,
+        STATE_PIECE_INSERT,
+        STATE_LOGO,
+    } State, Next_state
+    always_ff @ (posedge Clk)
+    begin
+        if(RESET)
+            State <= CLEARALL
+        else
+            State = Next_state;
+    end
+    always_comb
+    begin
+        //default signals go here
+        //this is where i learned about vscode ctrl+alt+up/down
+        //sorry it's ugly but it was very satisfying to write.
+        //they're all zero.
+        FALL = 0;
+        MOVELEFT = 0;
+        MOVERIGHT = 0;
+        KONAMI = 0;
+        NONEINPU = 0;
+        CLEARLINECHECK = 0;
+        CLEARLINEACT = 0;
+        CLEARLINE = 0;
+        PIECEPLACED = 0;
+        ROTATELEFT = 0;
+        ROTATELEFT_2 = 0;
+        ROTATERIGHT = 0;
+        ROTATERIGHT_2 = 0;
+        HOLDPIECE = 0;
+        HOLDPIECE_2 = 0;
+        HOLDPIECE_3 = 0;
+        unique case (State)
+                        //GAMEBOARD RELEVANT STATES
+            STATE_PIECEPLACED : // todo all
+            STATE_ROTATELEFT_1 :
+            STATE_ROTATELEFT_2 :
+            STATE_ROTATERIGHT :
+            STATE_ROTATERIGHT_2 :
+            STATE_FALL :
+            STATE_FALL_2 :
+            STATE_MOVELEFT :
+            STATE_MOVERIGHT :
+            STATE_KONAMI :
+            STATE_NONEINPUT :
+            STATE_CLEARLINECHECK :
+            STATE_CLEARLINEACT :
+            STATE_CLEARLINE :
+            STATE_HOLDPIECE :
+            STATE_HOLDPIECE_2 :
+            STATE_HOLDPIECE_3 :
+            STATE_ENDGAME :
+            STATE_CLEARALL :
+            //OTHER STATES :
+            STATE_PIECE_LOAD :
+            STATE_PIECE_INSERT :
+            STATE_LOGO :
+    end
+    
 
 
 endmodule
